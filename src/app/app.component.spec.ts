5de9234deca5a5acae01e19651b9fc4cf84dbef7
bamanczak <bamanczak@gmail.com>
2020-05-16 15:33:30 +0200

    Update app.component.spec.ts
    Zwiększyłem pokrycie kodu testami jednostkowymi

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -9,12 +9,43 @@ describe('AppComponent', () => {
     component = new AppComponent();
   }));
 
+  it('Aplikacja powinna akceptować prawidłowy PESEL 29 luty lata przestępne', () => {
+    expect(component.isValidPesel('20222912347')).toBe(true);
+  });
+
+  it('Aplikacja powinna odrzucić PESEL 29 luty lata nieprzestępne', () => {
+    expect(component.isValidPesel('19222912341')).toBe(false);
+  });
+
+  it('Aplikacja powinna odrzucić za krótki PESEL', () => {
+    expect(component.isValidPesel('6484299992')).toBe(false);
+  });
+
+/*
+ZNALEZIONY BŁĄD DZIĘKU SUGESTII STUDENTÓW
+  it('Aplikacja powinna odrzucić za długi PESEL', () => {
+    expect(component.isValidPesel('648429999222')).toBe(true);
+  });
+*/
+
+  it('Aplikacja powinna akceptować prawidłowy PESEL dla XIX wieku', () => {
+    expect(component.isValidPesel('64842999922')).toBe(true);
+  });
+
   it('Aplikacja powinna akceptować prawidłowy PESEL dla XX wieku', () => {
     expect(component.isValidPesel('64042999928')).toBe(true);
   });
 
   it('Aplikacja powinna akceptować prawidłowy PESEL dla XXI wieku', () => {
     expect(component.isValidPesel('64242999924')).toBe(true);
+  });
+
+  it('Aplikacja powinna akceptować prawidłowy PESEL dla XXII wieku', () => {
+    expect(component.isValidPesel('64442999920')).toBe(true);
+  });
+
+  it('Aplikacja powinna akceptować prawidłowy PESEL dla XXIII wieku', () => {
+    expect(component.isValidPesel('64642999926')).toBe(true);
   });
 
   it('Aplikacja powinna odrzucić PESEL z nieprawidłową cyfrą kontrolną', () => {
